2913ce210393a414f76829d1614cb2ca6088d148
Ryan Ross <ryan@reross.com>
2022-05-02 12:02:30 -0500

    fetched API and displayed result on page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import {Header, CharacterCard, Navbar} from './components'
 
 function App() {
+  const [chars, setChars] = useState([])
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character')
+      .then((res) => setChars(res.data.results))
+      .catch((err) => console.error(err))
+  }, [])
+
+
   return (
     <div className="App">
       <Header />
-      <CharacterCard/>
+      <div>
+ {chars.map((char) => (
+          <CharacterCard char={char} key={char.id}/>
+        ))}
+
+      </div>
       <Navbar />
-    </div>
+      </div>
   );
 }
 
